bb5219643f206c14c1f3985e17d9c4886288bc5e
Pariola <kehindephilip15@gmail.com>
2023-04-30 00:18:39 +0100

    ...feat: chat layout component

---
+++ src/layout/ChatLayout.jsx
@@ -0,0 +1,26 @@
+import PropTypes from 'prop-types';
+//
+import Sidebar from './Sidebar';
+
+// Base styles
+import styles from './layout.module.scss';
+
+ChatLayout.propTypes = {
+  onNewChat: PropTypes.element.isRequired,
+  children: PropTypes.element.isRequired,
+};
+
+export default function ChatLayout(props) {
+  return (
+    <div
+      className={`${styles.chatLayout} overflow-hidden w-[100vw] h-[100vh] relative`}
+    >
+      <Sidebar onNewChat={props.onNewChat} />
+      <div className="flex h-[100vh] max-w-[100vw] pl-[18%]">
+        <main className={`${styles.chatArena} relative h-[100vh] w-[100vw]`}>
+          {props.children}
+        </main>
+      </div>
+    </div>
+  );
+}
