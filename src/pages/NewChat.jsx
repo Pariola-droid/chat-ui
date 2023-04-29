e19ed04f06750841971549eba46d1cd52015406e
Pariola <kehindephilip15@gmail.com>
2023-04-30 00:19:00 +0100

    ...dummy new page (testing)

---
+++ src/pages/NewChat.jsx
@@ -0,0 +1,11 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import ChatLayout from '../layout/ChatLayout';
+
+export default function NewChat() {
+  return (
+    <div>
+      <ChatLayout>hey</ChatLayout>
+    </div>
+  );
+}
